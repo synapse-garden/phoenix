c96ac0683db316f359fc83e1ae34216562cbe59c
Bodie Solomon <binary132@gmail.com>
2014-08-22 02:15:20 -0400

    Added toggle function and mouse grab toggle to Inputter

--- src/main/scala/com/sg/pnx/Inputter.scala
+++ src/main/scala/com/sg/pnx/Inputter.scala
@@ -6,12 +6,19 @@ package com.sg.pnx
 
 import org.lwjgl.input.Mouse
 import org.lwjgl.input.Keyboard
-import collection.mutable.HashMap
+import collection.mutable
 import com.sg.pnx.graphics.util.DisplayUtil
 import com.sg.pnx.math.Math
 
 object Inputter {
-  val keysDown = HashMap[Int, Boolean](Keyboard.KEY_ESCAPE -> false).withDefaultValue(false)
+  private object KeyToggle extends Enumeration {
+    type KeyToggle = Value
+    val s0, s1, s2, s3 = Value
+  }
+
+  import KeyToggle._
+
+  val keysDown = mutable.HashMap[Int, Boolean]( Keyboard.KEY_ESCAPE -> false ).withDefaultValue(false)
 
   private var _timeNs: Long = 0
   private var _frameTime: Long = 0
@@ -26,7 +33,7 @@ object Inputter {
     _timeNs = System.nanoTime
   }
 
-  private def updateMouse( ) {
+  private def updateMouseGrabbed( ) {
     mouseDX = Mouse.getX - DisplayUtil.halfWidth
     mouseDY = Mouse.getY - DisplayUtil.halfHeight
     mouseX = mouseX + mouseDX
@@ -38,6 +45,36 @@ object Inputter {
     Mouse.setCursorPosition( DisplayUtil.halfWidth, DisplayUtil.halfHeight )
   }
 
+  private def unsetMouseMotion( ) {
+    mouseDX = 0
+    mouseDY = 0
+    mouseX = DisplayUtil.halfWidth
+    mouseY = DisplayUtil.halfHeight
+    mouseModX = 0
+    mouseModY = 0
+    mouseModDX = 0
+    mouseModDY = 0
+    Mouse.setCursorPosition( DisplayUtil.halfWidth, DisplayUtil.halfHeight )
+  }
+
+  private def toggle( state: KeyToggle, keyDown: Boolean ): KeyToggle = state match {
+    case `s0` => if(  keyDown ) s1 else s0
+    case `s1` => if( !keyDown ) s2 else s1
+    case `s2` => if(  keyDown ) s3 else s2
+    case `s3` => if( !keyDown ) s0 else s3
+  }
+
+  private def updateState( ) {
+    _mouseGrabbed = toggle( _mouseGrabbed, keysDown( Keyboard.KEY_ESCAPE ) )
+    if( _mouseGrabbed == s1 ) { unsetMouseMotion( ) }
+  }
+
+  // Die if you press ESC.
+  def exitRequested: Boolean = keysDown( Keyboard.KEY_Q )
+
+  private var _mouseGrabbed = s2
+  def mouseGrabbed = _mouseGrabbed == s1 || _mouseGrabbed == s2
+
   private var (_mouseX, _mouseY) = (0, 0)
   var (mouseDX, mouseDY) = (0, 0)
   var (mouseModDX: Float, mouseModDY: Float) = (0f, 0f)
@@ -45,14 +82,14 @@ object Inputter {
 
   def mouseX = _mouseX
   def mouseY = _mouseY
-  def mouseX_=( newX: Int ) = _mouseX = Math.clamp( newX, 0, DisplayUtil.width )
-  def mouseY_=( newY: Int ) = _mouseY = Math.clamp( newY, 0, DisplayUtil.width )
+  private def mouseX_=( newX: Int ) = _mouseX = Math.clamp( newX, 0, DisplayUtil.width )
+  private def mouseY_=( newY: Int ) = _mouseY = Math.clamp( newY, 0, DisplayUtil.width )
 
-  def exitRequested: Boolean = keysDown( Keyboard.KEY_ESCAPE )
   def timeNs: Long = _timeNs
   def frameTime: Long = _frameTime
 
   def init( ) {
+    println( "Press Q to quit, ESC to release or grab mouse" )
     mouseX = DisplayUtil.halfWidth
     mouseY = DisplayUtil.halfHeight
     mouseDX = mouseX
@@ -63,7 +100,8 @@ object Inputter {
 
   def update( ) {
     updateTime( )
-    while( Keyboard.next ) keysDown += Keyboard.getEventKey -> true
-    updateMouse( )
+    while( Keyboard.next ) keysDown += Keyboard.getEventKey -> Keyboard.getEventKeyState
+    updateState( )
+    if( mouseGrabbed ) updateMouseGrabbed( )
   }
 }
